80ef12b2d7e48c41954d44d74a4f3f7f3113dda0
JT Coates <wyosred@yahoo.com>
2022-05-23 23:47:05 -0600

    not rendering correctly, will fix

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,45 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    id:1,
+    name: "",
+    image: `https://i.ibb.co/2FsfXqM/stranger-things.png`,
+    season: 1,
+    number: 1,
+    summary: "Lorem Ipsum   ",
+    runtime: 1,
+}
 
-test("renders the summary test passed as prop", () => { });
+const testEpisodeWithoutImage = {
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "Lorem Ipsum   ",
+    runtime: 1,
+}
 
-test("renders default image when image is not defined", () => { });
+test("renders without error", () => {   
+    render (<Episode epsiode={testEpisode}/>);
+
+});
+
+test("renders the summary test passed as prop", () => { 
+    render (<Episode epsiode={testEpisode}/>);
+
+    const summary = screen.queryByText(/lorem ipsum/i)
+
+    expect (summary).toBeInTheDocument();
+    expect (summary).toBeTruthy();
+    expect(summary).toHaveTextContent("Lorem Ipsum")
+});
+
+test("renders default image when image is not defined", () => {
+    render (<Episode epsiode={testEpisodeWithoutImage}/>);
+    const image = screen.queryByAltText ('./stranger_things.png')
+    expect(image).toBeInTheDocument();
+
+
+ });
